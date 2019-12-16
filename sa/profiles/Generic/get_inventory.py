a34a39fca051ebfc17688ef4a9daa2cc6ea6a15d
bee26 <bee3026@gmail.com>
2019-12-17 00:25:37 +0300

    new_get_inventory_Generic_profile

---
+++ sa/profiles/Generic/get_inventory.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Generic.get_inventory
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetinventory import IGetInventory
+
+class Script(BaseScript):
+    name = "Generic.get_inventory"
+    interface = IGetInventory
+
+    def execute_snmp(self):
+        v = self.scripts.get_version()
+        try:
+            serial = v["attributes"]["Serial Number"]
+        except:
+            serial = None
+        return [
+            {
+                "type": "CHASSIS",
+                "vendor": [v["vendor"]],
+                "part_no": [v["platform"]],
+                "serial": serial
+            }
+        ]
