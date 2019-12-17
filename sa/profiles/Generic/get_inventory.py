0c624fc602f5816374e64ebde8500a5ba1cee57c
bee26 <bee26@ya.ru>
2019-12-17 06:20:24 +0000

    Update get_inventory.py

--- sa/profiles/Generic/get_inventory.py
+++ sa/profiles/Generic/get_inventory.py
@@ -12,18 +12,20 @@ from noc.sa.interfaces.igetinventory import IGetInventory
 
 
 class Script(BaseScript):
-    name = 'Generic.get_inventory'
+    name = "Generic.get_inventory"
     interface = IGetInventory
 
     def execute_snmp(self):
         v = self.scripts.get_version()
         try:
-            serial = v['attributes']['Serial Number']
+            serial = v["attributes"]["Serial Number"]
         except:
             serial = None
-        return [{
-            'type': 'CHASSIS',
-            'vendor': [v['vendor']],
-            'part_no': [v['platform']],
-            'serial': serial,
-            }]
+        return [
+            {
+                "type": "CHASSIS",
+                "vendor": [v["vendor"]],
+                "part_no": [v["platform"]],
+                "serial": serial,
+            }
+        ]
