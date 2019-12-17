a717da9db448f40c67140b6aa82e4beff922905c
Dmitry Volodin <dv@nocproject.org>
2019-12-17 08:34:57 +0300

    py3: Fix md5crypt

--- core/comp.py
+++ core/comp.py
@@ -8,6 +8,7 @@
 
 # Third-party modules
 import six
+from typing import List
 
 DEFAULT_ENCODING = "utf-8"
 
@@ -32,3 +33,33 @@ def smart_text(s, errors="strict"):
     if isinstance(s, six.binary_type):
         return s.decode(DEFAULT_ENCODING, errors=errors)
     return six.text_type(s)
+
+
+if six.PY3:
+
+    def bord(x):
+        # type: (int) -> int
+        return x
+
+    def bchr(x):
+        # type: (int) -> bytes
+        return bytes([x])
+
+    def make_bytes(x):
+        # type: (List[int]) -> bytes
+        return bytes(x)
+
+
+else:
+
+    def bord(x):
+        # type: (bytes) -> int
+        return ord(x)
+
+    def bchr(x):
+        # type: (bytes) -> bytes
+        return x
+
+    def make_bytes(x):
+        # type: (List[bytes]) -> bytes
+        return b"".join(x)
