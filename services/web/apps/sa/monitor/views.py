2fee2e4c1994df6113713010c8c765d86695913d
Andrey Vertiprahov <aversanta@gmail.com>
2019-12-28 14:29:19 +0500

    Use scheduler collection first for sa.monitor

--- services/web/apps/sa/monitor/views.py
+++ services/web/apps/sa/monitor/views.py
@@ -8,23 +8,16 @@
 
 # Python module
 import re
-import zlib
 import datetime
 
-# Third-party modules
-from django.db.models import Q as d_Q
-
-#  # NOC modules
-from noc.core.mongo.connection import get_db
+# NOC modules
 from noc.services.web.apps.sa.objectlist.views import ObjectListApplication
 from noc.core.dateutils import humanize_distance
 from noc.core.scheduler.scheduler import Scheduler
 from noc.sa.models.managedobject import ManagedObject
 from noc.core.scheduler.job import Job
-from noc.main.models.pool import Pool
+from noc.sa.models.profile import Profile
 from noc.core.translation import ugettext as _
-from noc.lib.app.extmodelapplication import ExtModelApplication
-from noc.sa.models.useraccess import UserAccess
 
 
 class MonitorApplication(ObjectListApplication):
@@ -35,94 +28,81 @@ class MonitorApplication(ObjectListApplication):
     title = _("Monitor")
     menu = _("Monitor")
     icon = "icon_monitor"
+    enable_paging = False
 
     rx_time = re.compile(r"Completed. Status: SUCCESS\s\((?P<time>\S+)\)", re.MULTILINE)
+    job_map = {
+        "noc.services.discovery.jobs.periodic.job.PeriodicDiscoveryJob": "p",
+        "noc.services.discovery.jobs.box.job.BoxDiscoveryJob": "b",
+    }
 
-    def get_job_data(self, scheduler, pool=None, status=None, key=None, jcls=None):
-        result = []
-        scheduler = Scheduler(name=scheduler, pool=pool)
-        q = {}
-        if status:
-            q["s"] = status
-        if key:
-            q.update({"jcls": jcls, "key": key})
-        for r in scheduler.get_collection().find(q):
-            res = r["key"]
-            result.append(res)
-        return result
+    def extra_data(self, data, ordering=None, start=None, limit=None):
+        # print(ordering, start, limit)
+        scheduler = Scheduler("discovery", pool="KAMCHATKA").get_collection()
+        pipeline = [
+            {"$match": {Job.ATTR_KEY: {"$in": list(data.values_list("id", flat=True))}}}
+        ]
+        pipeline += [
+            {
+                "$group": {
+                    "_id": "$key",
+                    "jobs": {
+                        "$push": {
+                            "jcls": "$jcls",
+                            "s": "$s",
+                            "ts": "$ts",
+                            "last": "$last",
+                            "ldur": "$ldur",
+                            "ls": "$ls",
+                        }
+                    },
+                }
+            }
+        ]
+        if start:
+            pipeline += [{"$skip": start}]
+        if limit:
+            pipeline += [{"$limit": limit}]
+        return scheduler.aggregate(pipeline)
 
-    def queryset(self, request, query=None):
+    def bulk_field_managed_object(self, data):
         """
-        Filter records for lookup
+        Apply managed objects field
+        :param data:
+        :return:
         """
-        if query:
-            self.logger.info("Queryset %s" % query)
-            if self.managed_filter:
-                q = d_Q(is_managed=True)
-            else:
-                q = d_Q()
-            if not request.user.is_superuser:
-                q &= UserAccess.Q(request.user)
-            sq = ManagedObject.get_search_Q(query)
-            if sq:
-                q &= sq
-            else:
-                q &= d_Q(name__contains=query)
-            return self.model.objects.filter(q)
-        status = request.POST.get("status")
-        if not status:
-            status = "R"
-        res = []
-        for p in Pool.objects.filter():
-            if p.name == "P0001":
-                continue
-            res += self.get_job_data("discovery", pool=p.name, status=status)
-        return self.model.objects.filter(id__in=res)
-
-    def get_data(self, job, job_key, prefix, mo_id):
-        time = "--"
-        last_success = humanize_distance(job["last"]) if "last" in job else "--"
-        last_status = job.get(Job.ATTR_LAST_STATUS)
-        time_start = job.get(Job.ATTR_TS)
-        status = job["s"] if "s" in job else "--"
-        if status == Job.S_WAIT:
-            key = "discovery-%s-%s" % (job_key, mo_id)
-            joblog = get_db()["noc.joblog"].find_one({"_id": key})
-            if joblog and joblog.get("log"):
-                res = ExtModelApplication.render_plain_text(zlib.decompress(str(joblog.get("log"))))
-                match = self.rx_time.search(str(res))
-                if match:
-                    s = match.group("time").split(".")[0]
-                    if len(s) > 3:
-                        time = "%s sec" % (int(s) / 1000)
-                    else:
-                        time = "%s mc" % s
-        return {
-            "%s_time_start" % prefix: datetime.datetime.strftime(time_start, "%d.%m.%Y %H:%M"),
-            "%s_last_success" % prefix: last_success,
-            "%s_status" % prefix: status,
-            "%s_time" % prefix: time,
-            "%s_duration" % prefix: humanize_distance(time_start),
-            "%s_last_status" % prefix: last_status,
+        mo_ids = [x["id"] for x in data]
+        if not mo_ids:
+            return data
+        mos = {
+            x[0]: {"name": x[1], "address": x[2], "profile_name": str(Profile.get_by_id(x[3]))}
+            for x in ManagedObject.objects.filter(id__in=mo_ids).values_list(
+                "id", "name", "address", "profile"
+            )
         }
+        for x in data:
+            x.update(mos[x["id"]])
+        return data
 
-    def instance_to_dict(self, mo, fields=None):
+    def instance_to_dict(self, data, fields=None):
         result = {
-            "id": str(mo.id),
-            "name": mo.name,
-            "address": mo.address,
-            "profile_name": mo.profile.name,
+            "id": data["_id"],
+            "name": "",
+            "address": "",
+            "profile_name": "",
         }
-        box_jcls = "noc.services.discovery.jobs.box.job.BoxDiscoveryJob"
-        periodic_jcls = "noc.services.discovery.jobs.periodic.job.PeriodicDiscoveryJob"
-        box_job = Job.get_job_data("discovery", jcls=box_jcls, key=mo.id, pool=mo.pool.name)
-        periodic_job = Job.get_job_data(
-            "discovery", jcls=periodic_jcls, key=mo.id, pool=mo.pool.name
-        )
-        if box_job:
-            res = self.get_data(box_job, box_jcls, "b", mo.id)
-            result.update(res)
-        if periodic_job:
-            res = self.get_data(periodic_job, periodic_jcls, "p", mo.id)
-            result.update(res)
+        for job in data["jobs"]:
+            prefix = self.job_map[job["jcls"]]
+            result.update(
+                {
+                    "%s_time_start"
+                    % prefix: datetime.datetime.strftime(job[Job.ATTR_TS], "%d.%m.%Y %H:%M"),
+                    "%s_last_success" % prefix: job.get(Job.ATTR_LAST),
+                    "%s_status" % prefix: job[Job.ATTR_STATUS] if Job.ATTR_STATUS in job else "--",
+                    "%s_time" % prefix: job[Job.ATTR_LAST_DURATION],
+                    "%s_duration" % prefix: humanize_distance(job[Job.ATTR_TS]),
+                    "%s_last_status" % prefix: job.get(Job.ATTR_LAST_STATUS),
+                }
+            )
+
         return result
