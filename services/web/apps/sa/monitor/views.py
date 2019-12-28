b85ad0fd1b55b1ffaebf4eee6f4b50ca48bfd293
Andrey Vertiprahov <aversanta@gmail.com>
2019-12-28 11:15:10 +0500

    Fix PEP8.

--- services/web/apps/sa/monitor/views.py
+++ services/web/apps/sa/monitor/views.py
@@ -2,25 +2,28 @@
 # ---------------------------------------------------------------------
 # sa.monitor application
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
+
 # Python module
 import re
 import zlib
 import datetime
+
 # Third-party modules
 from django.db.models import Q as d_Q
+
 #  # NOC modules
-from noc.lib.nosql import get_db
+from noc.core.mongo.connection import get_db
 from noc.services.web.apps.sa.objectlist.views import ObjectListApplication
-from noc.lib.dateutils import humanize_distance
+from noc.core.dateutils import humanize_distance
 from noc.core.scheduler.scheduler import Scheduler
 from noc.sa.models.managedobject import ManagedObject
 from noc.core.scheduler.job import Job
 from noc.main.models.pool import Pool
 from noc.core.translation import ugettext as _
-from noc.lib.app.modelapplication import ModelApplication
+from noc.lib.app.extmodelapplication import ExtModelApplication
 from noc.sa.models.useraccess import UserAccess
 
 
@@ -40,10 +43,11 @@ class MonitorApplication(ObjectListApplication):
             name=scheduler,
             pool=pool,
         )
+        q = {}
         if status:
-            q = {"s": status}
+            q["s"] = status
         if key:
-            q = {"jcls": jcls, "key": key}
+            q.update({"jcls": jcls, "key": key})
         for r in scheduler.get_collection().find(q):
             res = r["key"]
             result.append(res)
@@ -87,7 +91,9 @@ class MonitorApplication(ObjectListApplication):
             key = "discovery-%s-%s" % (job_key, mo_id)
             joblog = get_db()["noc.joblog"].find_one({"_id": key})
             if joblog and joblog.get("log"):
-                res = ModelApplication.render_plain_text(zlib.decompress(str(joblog.get("log"))))
+                res = ExtModelApplication.render_plain_text(
+                    zlib.decompress(str(joblog.get("log")))
+                )
                 match = self.rx_time.search(str(res))
                 if match:
                     s = match.group("time").split(".")[0]
@@ -105,7 +111,6 @@ class MonitorApplication(ObjectListApplication):
         }
 
     def instance_to_dict(self, mo, fields=None):
-
         result = {
             'id': str(mo.id),
             'name': mo.name,
@@ -114,19 +119,10 @@ class MonitorApplication(ObjectListApplication):
         }
         box_jcls = "noc.services.discovery.jobs.box.job.BoxDiscoveryJob"
         periodic_jcls = "noc.services.discovery.jobs.periodic.job.PeriodicDiscoveryJob"
-
-        box_job = Job.get_job_data("discovery",
-                                   jcls=box_jcls,
-                                   key=mo.id,
-                                   pool=mo.pool.name
-                                   )
-
-        periodic_job = Job.get_job_data("discovery",
-                                        jcls=periodic_jcls,
-                                        key=mo.id,
-                                        pool=mo.pool.name
-                                        )
-
+        box_job = Job.get_job_data("discovery", jcls=box_jcls, key=mo.id, pool=mo.pool.name)
+        periodic_job = Job.get_job_data(
+            "discovery", jcls=periodic_jcls,key=mo.id,pool=mo.pool.name
+        )
         if box_job:
             res = self.get_data(box_job, box_jcls, "b", mo.id)
             result.update(res)
