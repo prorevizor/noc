9f588f1eaa19922b12e6664df903940388a2d88a
Your Name <aversanta@gmail.com>
2020-02-14 00:12:03 +0500

    Cisco.IOS.get_lacp_neighbors. Fix rx_sys_id regex.

--- sa/profiles/Cisco/IOS/get_lacp_neighbors.py
+++ sa/profiles/Cisco/IOS/get_lacp_neighbors.py
@@ -27,7 +27,7 @@ class Script(BaseScript):
         r"Partner's information:(?P<part_info>.+)\n",
         re.IGNORECASE | re.DOTALL,
     )
-    rx_sys_id = re.compile(r",(?P<sys_id>\w{4}\.\w{4}\.\w{4})")
+    rx_sys_id = re.compile(r",\s*(?P<sys_id>\w{4}\.\w{4}\.\w{4})")
 
     def execute(self):
         r = []
