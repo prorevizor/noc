9d3bfacd6b5ed1557794fde4a2006d4137ffd4b0
Your Name <aversanta@gmail.com>
2020-04-10 15:59:00 +0500

    Replace rb.

--- sa/profiles/Juniper/EX2500/profile.py
+++ sa/profiles/Juniper/EX2500/profile.py
@@ -26,6 +26,6 @@ class Profile(BaseProfile):
 
     rogue_chars = [
         "\r",
-        re.compile(b"\n\n.*?\n"),
-        re.compile(b"\n.*?:TELNET-ALERT:.*?\nvia TELNET/SSH From host .*?\n"),
+        re.compile(br"\n\n.*?\n"),
+        re.compile(br"\n.*?:TELNET-ALERT:.*?\nvia TELNET/SSH From host .*?\n"),
     ]
