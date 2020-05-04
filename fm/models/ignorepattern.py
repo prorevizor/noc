8003e34767cf9c4dca1902f411be18181b9a799b
smile13 <sysfar@gmail.com>
2020-05-04 10:58:01 +0300

    Add source unique

--- fm/models/ignorepattern.py
+++ fm/models/ignorepattern.py
@@ -14,7 +14,7 @@ from mongoengine.fields import StringField, BooleanField
 class IgnorePattern(Document):
     meta = {"collection": "noc.fm.ignorepatterns", "strict": False, "auto_create_index": False}
 
-    source = StringField()
+    source = StringField(unique=True)
     pattern = StringField(unique=True)
     is_active = BooleanField()
     description = StringField(required=False)
