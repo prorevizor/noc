ceefcdb7c63de284ec9ad17684d532098a06e01a
uncol <d.a.zakharov@gmail.com>
2020-06-05 07:51:07 +0300

    add comment

--- services/login/logged.py
+++ services/login/logged.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
 # Authentication handler
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -15,6 +15,9 @@ from .auth import AuthRequestHandler
 
 class IsLoggedRequestHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
+        """
+        Check secure cookie
+        """
         user = self.get_secure_cookie(AuthRequestHandler.USER_COOKIE)
         self.set_status(200, "OK")
         if user:
