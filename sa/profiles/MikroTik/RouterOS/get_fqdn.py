cbc90456214efffdec9c939bd96cfc888afae892
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-06-23 14:21:06 +0300

    Add SNMP support for MikroTik.RouterOS.get_fqdn script

--- sa/profiles/MikroTik/RouterOS/get_fqdn.py
+++ sa/profiles/MikroTik/RouterOS/get_fqdn.py
@@ -1,12 +1,12 @@
 # ---------------------------------------------------------------------
 # MikroTik.RouterOS.get_fqdn
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_fqdn import Script as BaseScript
 from noc.sa.interfaces.igetfqdn import IGetFQDN
 
 
@@ -14,9 +14,8 @@ class Script(BaseScript):
     name = "MikroTik.RouterOS.get_fqdn"
     interface = IGetFQDN
 
-    def execute(self):
+    def execute_cli(self):
         s = self.cli("/system identity print")
         if s and "name: " in s:
             return s.split(":", 1)[1].strip()
-        else:
-            return "MikroTik"
+        return "MikroTik"
