3ce7ace6e22e5e342f7469af9f1e5cde7a5e34fc
smile13 <sysfar@gmail.com>
2020-07-03 14:51:06 +0300

    Update

--- services/discovery/jobs/periodic/alarms.py
+++ services/discovery/jobs/periodic/alarms.py
@@ -2,13 +2,14 @@
 # ----------------------------------------------------------------------
 # Alarms Check
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
 # Python modules
 import time
 import datetime
+
 # NOC modules
 from noc.core.nsq.pub import nsq_pub
 from noc.services.discovery.jobs.base import DiscoveryCheck
@@ -20,6 +21,7 @@ class AlarmsCheck(DiscoveryCheck):
     """
     CPE alarm discovery
     """
+
     name = "alarms"
 
     required_script = "get_alarms"
@@ -29,45 +31,58 @@ class AlarmsCheck(DiscoveryCheck):
         self.logger.info("Checking Alarm CPEs")
         result = self.object.scripts.get_alarms()  # result get_alarms
         for r in result:
-            if "object_id" in r:
-                mos += [self.find_cpe(r["object_id"], self.object.id)]
+            if "path" in r:
+                mos += [self.find_cpe(r["path"][0], self.object.id)]
             else:
                 mos += [self.object]
         mos_id = list(set(mo.id for mo in mos if mo))
         # Controller Alarm
-        objcet_alarms = {str(cpe["alarm_id"]): cpe for cpe in result}
+        objcet_alarms = {str(cpe["id"]): cpe for cpe in result}
         # System Events
-        system_alarms = {str(a.raw_vars["alarm_id"]): a for a in ActiveEvent.objects.filter(managed_object__in=mos_id,
-                                                                                            source="other",
-                                                                                            raw_vars__alarm_id__exists=True,
-                                                                                            raw_vars__status="Open")}
+        system_alarms = {
+            str(a.raw_vars["id"]): a
+            for a in ActiveEvent.objects.filter(
+                managed_object__in=mos_id,
+                source="other",
+                raw_vars__id__exists=True,
+                raw_vars__status="Open",
+            )
+        }
         # Search objcet alarms in system events, if objcet alarms not in system events, create!
         create_objcet_alarms = set(objcet_alarms.keys()).difference(set(system_alarms.keys()))
         # Search system events in objcet alarms, if system events not in objcet alarms, close event!
         close_objects_event = set(system_alarms.keys()).difference(set(objcet_alarms.keys()))
         # If not new/old alarms, return.
         if len(create_objcet_alarms) == 0 and len(close_objects_event) == 0:
-            self.logger.debug("No New or Old Alarms")
+            self.logger.debug("Is no New/Old alarms")
             return
         if len(create_objcet_alarms) != 0:
             for new_event in create_objcet_alarms:
-                if "object_id" in objcet_alarms[new_event]:
-                    managed_object = self.find_cpe(objcet_alarms[new_event]["object_id"], self.object.id)
+                if "path" in objcet_alarms[new_event]:
+                    managed_object = self.find_cpe(
+                        objcet_alarms[new_event]["path"][0], self.object.id
+                    )
                     if not managed_object:
                         managed_object = self.object
-                        self.logger.warning("No object %s for alarm: \n%s" % (objcet_alarms[new_event]["object_id"],
-                                                                              objcet_alarms[new_event]))
+                        self.logger.warning(
+                            "No object %s for alarm: \n%s"
+                            % (objcet_alarms[new_event]["path"][0], objcet_alarms[new_event])
+                        )
                 else:
                     managed_object = self.object
                 raw_vars = objcet_alarms[new_event]
+                if len(raw_vars["path"]) > 1 and raw_vars["path"][1]:
+                    raw_vars["slot"] = raw_vars["path"][1]
+                if len(raw_vars["path"]) > 2 and raw_vars["path"][2]:
+                    raw_vars["interface"] = raw_vars["path"][2]
                 raw_vars["status"] = "Open"
-                self.raise_event(self.logger, managed_object.id, managed_object.pool.name, raw_vars)
+                self.raise_event(managed_object.id, managed_object.pool.name, raw_vars)
         if len(close_objects_event) != 0:
             for close_event in close_objects_event:
                 event = system_alarms[close_event]
                 raw_vars = event.raw_vars
                 raw_vars["status"] = "Close"
-                self.raise_event(self.logger, event.managed_object.id, event.managed_object.pool.name, raw_vars)
+                self.raise_event(event.managed_object.id, event.managed_object.pool.name, raw_vars)
                 event.mark_as_archived("Close event")
                 self.logger.info("Close event %s" % event)
 
@@ -78,15 +93,9 @@ class AlarmsCheck(DiscoveryCheck):
         except ManagedObject.DoesNotExist:
             return None
 
-    @classmethod
-    def raise_event(self, log, mo_id, pool_name, raw_vars):
-        d = datetime.datetime.strptime(raw_vars["alarm_time"], '%Y-%m-%dT%H:%M:%S')
+    def raise_event(self, mo_id, pool_name, raw_vars):
+        d = datetime.datetime.strptime(raw_vars["ts"], "%Y-%m-%dT%H:%M:%S")
         ts = time.mktime(d.timetuple())
-        msg = {
-            "ts": ts,
-            "object": mo_id,
-            "source": "other",
-            "data": raw_vars
-        }
-        log.info("Pub Event: %s", msg)
+        msg = {"ts": ts, "object": mo_id, "source": "other", "data": raw_vars}
+        self.logger.info("Pub Event: %s", msg)
         nsq_pub("events.%s" % pool_name, msg)
