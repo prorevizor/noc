c4da77b0d5225052fc7cbe4894809c7ed2303ae7
Dmitry Volodin <dv@nocproject.org>
2020-08-18 11:34:14 +0300

    Fix

--- core/confdb/normalizer/base.py
+++ core/confdb/normalizer/base.py
@@ -9,7 +9,7 @@
 import itertools
 from collections import defaultdict
 from functools import partial
-from typing import Optional, List
+from typing import List
 
 # NOC modules
 from noc.core.ip import IPv4
