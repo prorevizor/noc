47a3828d842830d59942d2691acd56262a896a95
maksmile13 <sysfar@gmail.com>
2020-08-18 16:11:17 +0300

    Fix if elif

--- lib/app/extapplication.py
+++ lib/app/extapplication.py
@@ -207,11 +207,10 @@ class ExtApplication(Application):
             fav_items = self.get_favorite_items(request.user)
             if fs:
                 data = data.filter(id__in=fav_items)
-            else:
-                if isinstance(data, QuerySet):  # Model
-                    data = data.exclude(id__in=fav_items)
-                else:  # Doc
-                    data = data.filter(id__nin=fav_items)
+            elif not fs and isinstance(data, QuerySet):  # Model
+                data = data.exclude(id__in=fav_items)
+            else:  # Doc
+                data = data.filter(id__nin=fav_items)
         # Store unpaged/unordered queryset
         unpaged_data = data
         # Select related records when fetching for models
