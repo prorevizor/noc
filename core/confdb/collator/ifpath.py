fa53890a1e74c104bc33d08e4eff214b044ac339
Your Name <aversanta@gmail.com>
2020-09-06 20:57:33 +0500

    Add stack processed.

--- core/confdb/collator/ifpath.py
+++ core/confdb/collator/ifpath.py
@@ -27,7 +27,11 @@ logger = logging.getLogger(__name__)
 
 class IfPathCollator(BaseCollator):
     """
-    Direct map between connection name and interface name
+    Direct map between connection name and interface name by it path.
+    1) - split SA interface name on path_component by name_path method
+    2) - iterate over component on Inventory Item path from end to start
+    3) - Compare SA path and Inventory item path by component. If equal - return interface
+    4) - If more that one interfaces has equal path, but different if_type, use protocols for detect right
     """
 
     def __init__(self):
@@ -36,7 +40,7 @@ class IfPathCollator(BaseCollator):
         self.paths = defaultdict(list)
 
     PROTOCOL_MAPPING = {
-        "Et": {
+        "et": {
             "1000BASET",
             "1000BASETX",
             "100BASETX",
@@ -45,37 +49,46 @@ class IfPathCollator(BaseCollator):
             "TransEth1G",
             "TransEth10G",
         },  # Ethernet
-        "ME": {"100BASETX"},
-        "Fa": {"100BASETX", "10BASET", "TransEth100M"},  # FastEthernet
-        "Fo": {"TransEth40G"},  # FortyGigabitEthernet
-        "Gi": {
-            "10BASET",
-            "1000BASET",
-            "1000BASETX",
-            "TransEth100M",
-            "TransEth1G",
-        },  # GigabitEthernet
-        "Te": {"TransEth10G"},  # TenGigabitEthernet
-        "XG": {"TransEth1G", "TransEth10G"},
+        "me": {"10BASET", "100BASETX"},
+        "m-": {"10BASET", "100BASETX"},
+        "fa": {"100BASETX", "10BASET", "TransEth100M"},  # FastEthernet
+        "fo": {"TransEth40G"},  # FortyGigabitEthernet
+        "gi": {"1000BASET", "1000BASETX", "TransEth1G",},  # GigabitEthernet
+        "te": {"TransEth10G"},  # TenGigabitEthernet
+        "xg": {"TransEth10G"},
     }
 
     # TransEth1G,TransEth10G
     def get_protocols(self, if_name: str) -> Set[str]:
-        return self.PROTOCOL_MAPPING.get(if_name[:2])
+        """
+        Getting protocols by ifname.
+        :param if_name: Interface name
+        :return:
+        """
+        return self.PROTOCOL_MAPPING.get(if_name.lower()[:2])
 
     @staticmethod
-    def name_path(if_name: str,) -> Tuple[Union[str, None], List[str], Union[str, None]]:
+    def name_path(if_name: str) -> Tuple[Union[str, None], List[str], Union[str, None]]:
+        """
+        Split Interface Name by path component: if_type, if_path, if_num.
+         Example Gi 1/0/2: if_type: Gi, if_path: 1/0, if_num: 2;
+         10: if_type: None, if_path: [], if_num: 10
+        :param if_name:
+        :return:
+        """
         match = rx_ifname_splitter.match(if_name)
         if not match:
             return None, [], None
         if_type, if_path, if_num = match.groups()
         if if_path:
-            if_path = rx_ifpath_splitter.findall(if_name[match.end(1) : match.start(3)])
+            if_path = rx_ifpath_splitter.findall(
+                if_name[match.end(1) if match.group(1) else 0 : match.start(3)]
+            )
         return if_type, if_path or [], if_num
 
     def iter_path_component(self, path) -> str:
         """
-        Split PathItem list
+        Iterator by path_component in PathItem. Return path component from end to start
         :param path:
         :return:
         """
@@ -83,18 +96,19 @@ class IfPathCollator(BaseCollator):
             return None
         for item in reversed(path):
             _, c_path, c_num = self.name_path(item.connection.name)
-            # if len(c_path) >= 2:
+            yield c_num
+            # if len(c_path) > 2:
             #     # Remove first element
             #     # c_path.pop(0)
             #     # Absolute path
-            #     continue
-            for cp in c_path:
+            #     logger.warning("On slot name use absolute path. Strip first")
+            #     c_path = c_path[1:]
+            for cp in reversed(c_path):
                 if not cp.isdigit():
                     # if not number - skipping (X/1/1)
                     logger.warning("Path component '%s' is not digit. Skipping..", cp)
                     continue
                 yield cp
-            yield c_num
         if item.object.get_data("stack", "stackable"):
             yield item.object.get_data("stack", "member")
 
@@ -103,7 +117,7 @@ class IfPathCollator(BaseCollator):
         if not self.paths:
             # SA interface path map
             for if_name, iface in interfaces.items():
-                if iface.type != "physical":
+                if iface.type not in {"physical", "management"}:
                     # Physical scope
                     continue
                 if_type, if_path, if_num = self.name_path(if_name)
@@ -118,34 +132,49 @@ class IfPathCollator(BaseCollator):
 
         if_type, if_path, if_num = self.name_path(physical_path[-1].connection.name)
         if if_num not in self.paths:
-            logger.warning("Interface number %s is not in mapping", if_num)
+            logger.warning("Interface number %s is not in SA paths", if_num)
             return None
 
-        if if_path:
-            # @todo stack check, perhaps move to other collator
-            paths_candidate.append(tuple(if_path))
-
-        candidates = [x for x in self.paths[if_num]]
-        for num, step in enumerate(self.iter_path_component(physical_path[:-1]), start=1):
-            candidates = list(
-                filter(
-                    lambda x: (len(x[0]) < num) or (len(x[0]) >= num and x[0][-num] == step),
-                    candidates,
+        if physical_path[:-1]:
+            candidates = [x for x in self.paths[if_num]]
+            for step, num in enumerate(self.iter_path_component(physical_path[:-1]), start=1):
+                candidates = list(
+                    filter(
+                        lambda x: (len(x[0]) < step) or (len(x[0]) >= step and x[0][-step] == num),
+                        candidates,
+                    )
                 )
+            paths_candidate.append(tuple(candidates[0][0]))
+
+        if if_path and physical_path[0].object.get_data("stack", "member"):
+            # @todo perhaps move to other collator
+            paths_candidate.append(
+                tuple([physical_path[0].object.get_data("stack", "member")] + if_path[1:])
             )
-        paths_candidate.append(tuple(candidates[0][0]))
+        elif if_path:
+            paths_candidate.append(tuple(if_path))
+            # print(physical_path[0].object.get_data("stack", "member"))
+            # paths_candidate.append(tuple(reversed(tuple(self.iter_path_component([physical_path[-1]]))[1:])))
 
         logger.debug(
-            "Path candidates: %s, protocols: %s",
+            "Path candidates: %s, protocols: %s, (stack %s)",
             paths_candidate,
             physical_path[-1].connection.protocols,
+            physical_path[0].object.get_data("stack", "member"),
         )
         if_proto = set(physical_path[-1].connection.protocols)
         for p, if_name, protocols in self.paths[if_num]:
             if p in paths_candidate:
-                if protocols and if_proto - protocols:
-                    logger.info("Interface proto not coverage models: %s", if_proto - protocols)
+                if not protocols or not if_proto.intersection(protocols):
+                    # Protocols used for filter iface_type with equal path
+                    # @todo empty protocols
+                    logger.info("Interface proto not coverage models: %s:%s", if_proto, protocols)
                     continue
+                # self.paths[if_num].remove((p, if_name, protocols))
+                return if_name
+            elif not paths_candidate and len(self.paths[if_num]):
+                # USe for if_num only format
+                # self.paths[if_num].remove((p, if_name, protocols))
                 return if_name
 
         return None
--- tests/confdb/collator/test_ifpath.py
+++ tests/confdb/collator/test_ifpath.py
@@ -26,6 +26,7 @@ INTERFACES1 = [
     ("GigabitEthernet0/0/10", "physical"),
     ("XGigabitEthernet0/1/1", "physical"),
     ("XGigabitEthernet0/1/2", "physical"),
+    ("Ethernet0/0/1", "physical"),
     ("M-Ethernet0/0/1", "physical"),
 ]
 
@@ -44,15 +45,208 @@ PATHS1 = [
         ],
         "XGigabitEthernet0/1/2",
     ],
+    # [
+    #     [{"object": ("MockObject1", {}), "connection": ("MEth0/0/1", ["10BASET", "100BASETX"])}],
+    #     "M-Ethernet0/0/1",
+    # ],
     [
-        [{"object": ("MockObject1", {}), "connection": ("MEth0/0/1", ["10BASET", "100BASETX"])}],
-        "M-Ethernet0/0/1",
+        [{"object": ("MockObject1", {}), "connection": ("GigabitEthernet0/0/1", ["TransEth1G"]),}],
+        "GigabitEthernet0/0/1",
+    ],
+    [
+        [{"object": ("MockObject1", {}), "connection": ("GigabitEthernet0/0/2", ["TransEth1G"]),}],
+        "GigabitEthernet0/0/2",
+    ],
+    [
+        [{"object": ("MockObject1", {}), "connection": ("GigabitEthernet0/0/3", ["TransEth1G"]),}],
+        "GigabitEthernet0/0/3",
     ],
     [
         [
             {
                 "object": ("MockObject1", {}),
-                "connection": ("GigabitEthernet0/0/1", ["TransEth100M", "TransEth1G"]),
+                "connection": ("Ethernet0/0/1", ["10BASET", "100BASETX"]),
+            }
+        ],
+        "Ethernet0/0/1",
+    ],
+]
+
+# Raisecom paths
+INTERFACES2 = [
+    ("gigaethernet1/1/1", "physical"),
+    ("gigaethernet1/1/2", "physical"),
+    ("gigaethernet1/1/3", "physical"),
+    ("gigaethernet1/1/28", "physical"),
+    ("fastethernet1/0/1", "physical"),
+]
+
+PATHS2 = [
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("1/1/1", ["10BASET", "100BASETX", "1000BASETX"]),
+            }
+        ],
+        "gigaethernet1/1/1",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("1/1/2", ["10BASET", "100BASETX", "1000BASETX"]),
+            }
+        ],
+        "gigaethernet1/1/2",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("1/1/3", ["10BASET", "100BASETX", "1000BASETX"]),
+            }
+        ],
+        "gigaethernet1/1/3",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("1/1/28", ["10BASET", "100BASETX", "1000BASETX"]),
+            }
+        ],
+        "gigaethernet1/1/28",
+    ],
+    [
+        [{"object": ("MockObject1", {}), "connection": ("1/0/1", ["10BASET", "100BASETX"]),}],
+        "fastethernet1/0/1",
+    ],
+]
+
+# Qtech paths
+INTERFACES3 = [
+    ("Ethernet1/0/1", "physical"),
+    ("Ethernet1/0/2", "physical"),
+    ("Ethernet1/0/3", "physical"),
+    ("Ethernet1/0/28", "physical"),
+]
+
+PATHS3 = [
+    [
+        [{"object": ("MockObject1", {}), "connection": ("1", ["10BASET", "100BASETX"]),}],
+        "Ethernet1/0/1",
+    ],
+    [
+        [{"object": ("MockObject1", {}), "connection": ("2", ["10BASET", "100BASETX"]),}],
+        "Ethernet1/0/2",
+    ],
+    [
+        [{"object": ("MockObject1", {}), "connection": ("3", ["10BASET", "100BASETX"]),}],
+        "Ethernet1/0/3",
+    ],
+    [
+        [{"object": ("MockObject1", {}), "connection": ("sfp28", ["TransEth1G"]),}],
+        "Ethernet1/0/28",
+    ],
+]
+
+# Eltex PATH
+
+INTERFACES4 = [
+    ("Gi 1/0/1", "physical"),
+    ("Gi 1/0/2", "physical"),
+    ("Gi 1/0/3", "physical"),
+    ("Te 1/0/1", "physical"),
+    ("Gi 2/0/1", "physical"),
+    ("Gi 2/0/2", "physical"),
+    ("Gi 2/0/3", "physical"),
+    ("Te 2/0/1", "physical"),
+    ("Gi 3/0/1", "physical"),
+    ("Gi 3/0/2", "physical"),
+    ("Gi 3/0/3", "physical"),
+    ("Te 3/0/1", "physical"),
+]
+
+PATHS4 = [
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("gigabitethernet 1/0/1", ["TransEth100M", "TransEth1G"]),
+            }
+        ],
+        "Gi 1/0/1",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("gigabitethernet 1/0/2", ["TransEth100M", "TransEth1G"]),
+            }
+        ],
+        "Gi 1/0/2",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("gigabitethernet 1/0/3", ["TransEth100M", "TransEth1G"]),
+            }
+        ],
+        "Gi 1/0/3",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {}),
+                "connection": ("tengigabitethernet 1/0/1", ["TransEth10G"]),
+            }
+        ],
+        "Te 1/0/1",
+    ],
+]
+
+# Stack
+
+INTERFACES5 = [
+    ("GigabitEthernet0/0/1", "physical"),
+    ("GigabitEthernet0/0/2", "physical"),
+    ("GigabitEthernet1/0/1", "physical"),
+    ("GigabitEthernet1/0/2", "physical"),
+    ("GigabitEthernet2/0/1", "physical"),
+    ("GigabitEthernet2/0/2", "physical"),
+    ("XGigabitEthernet0/1/1", "physical"),
+    ("XGigabitEthernet0/1/2", "physical"),
+    ("M-Ethernet0/0/1", "physical"),
+]
+
+PATHS5 = [
+    [
+        [
+            {
+                "object": ("MockObject1", {"stack": {"stackable": "true", "member": "0"}}),
+                "connection": ("1", ""),
+            },
+            {"object": ("Connection1", {}), "connection": ("xfp 1", ["TransEth10G"])},
+        ],
+        "XGigabitEthernet0/1/1",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {"stack": {"stackable": "true", "member": "0"}}),
+                "connection": ("1", ""),
+            },
+            {"object": ("Connection1", {}), "connection": ("xfp 2", ["TransEth10G"])},
+        ],
+        "XGigabitEthernet0/1/2",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {"stack": {"stackable": "true", "member": "0"}}),
+                "connection": ("GigabitEthernet0/0/1", ["TransEth1G"]),
             }
         ],
         "GigabitEthernet0/0/1",
@@ -60,8 +254,8 @@ PATHS1 = [
     [
         [
             {
-                "object": ("MockObject1", {}),
-                "connection": ("GigabitEthernet0/0/2", ["TransEth100M", "TransEth1G"]),
+                "object": ("MockObject1", {"stack": {"stackable": "true", "member": "0"}}),
+                "connection": ("GigabitEthernet0/0/2", ["TransEth1G"]),
             }
         ],
         "GigabitEthernet0/0/2",
@@ -69,13 +263,23 @@ PATHS1 = [
     [
         [
             {
-                "object": ("MockObject1", {}),
-                "connection": ("GigabitEthernet0/0/3", ["TransEth100M", "TransEth1G"]),
+                "object": ("MockObject1", {"stack": {"stackable": "true", "member": "1"}}),
+                "connection": ("GigabitEthernet0/0/1", ["TransEth1G"]),
             }
         ],
-        "GigabitEthernet0/0/3",
+        "GigabitEthernet1/0/1",
+    ],
+    [
+        [
+            {
+                "object": ("MockObject1", {"stack": {"stackable": "true", "member": "1"}}),
+                "connection": ("GigabitEthernet0/0/2", ["TransEth1G"]),
+            }
+        ],
+        "GigabitEthernet1/0/2",
     ],
 ]
+
 
 PathItem = namedtuple("PathItem", ["object", "connection"])
 
@@ -86,7 +290,8 @@ class MockObject(object):
         self.data = data
 
     def get_data(self, interface, key):
-        pass
+        v = self.data.get(interface, {})
+        return v.get(key)
 
 
 class MockObjectConnection(object):
@@ -102,7 +307,16 @@ class MockInterface(object):
         self.type = type
 
 
-@pytest.mark.parametrize("interfaces,paths", [(INTERFACES1, PATHS1)])
+@pytest.mark.parametrize(
+    "interfaces,paths",
+    [
+        (INTERFACES1, PATHS1),
+        (INTERFACES2, PATHS2),
+        (INTERFACES3, PATHS3),
+        (INTERFACES4, PATHS4),
+        (INTERFACES5, PATHS5),
+    ],
+)
 def test_profile(interfaces, paths):
     ifaces = {}
     for iface_name, iface_type in interfaces:
