73799c0e40da2da4f7d9b633798a2012d72f9afb
Your Name <aversanta@gmail.com>
2020-09-16 15:13:29 +0500

    Fix black.

--- core/confdb/applicator/systemaaasourceaddresslookup.py
+++ core/confdb/applicator/systemaaasourceaddresslookup.py
@@ -11,7 +11,7 @@ from .query import QueryApplicator
 
 class DefaultAAASourceAddressLookupApplicator(QueryApplicator):
     """
-    Apply source-ip if default-interface set
+    Lookup source-address hints from default-interface
     """
 
     CHECK_QUERY = (
@@ -19,7 +19,7 @@ class DefaultAAASourceAddressLookupApplicator(QueryApplicator):
         " Match('hints', 'system', 'aaa', 'service-type', stype, 'default-interface')"
     )
     QUERY = [
-        # Getting source-ip from default-interface or default-address
+        # Getting source-ip from interface address by default-interface
         "((Match('hints', 'system', 'aaa', 'service-type', stype, 'default-interface', interface) and "
         " Match('virtual-router', VR, 'forwarding-instance', FI, 'interfaces', interface, 'unit', unit, 'inet', 'address', source)) and "
         " Fact('hints', 'system', 'aaa', 'service-type', stype, 'default-address', source))"
--- core/confdb/syntax/system/aaa/hints.py
+++ core/confdb/syntax/system/aaa/hints.py
@@ -7,17 +7,36 @@
 
 # NOC modules
 from ...defs import DEF
-from ...patterns import IF_NAME, IP_ADDRESS
+from ...patterns import IF_NAME, IP_ADDRESS, CHOICES
 
 HINTS_SYSTEM_AAA = DEF(
     "aaa",
     [
         DEF(
-            "default-address", [DEF(IP_ADDRESS, name="ip", gen="make_system_aaa_default_address")],
-        ),
-        DEF(
-            "default-interface",
-            [DEF(IF_NAME, name="interface", gen="make_system_aaa_default_interface")],
+            "service-type",
+            [
+                DEF(
+                    CHOICES("local", "radius", "tacacs+", "ldap", "ad"),
+                    [
+                        DEF(
+                            "default-address",
+                            [DEF(IP_ADDRESS, name="ip", gen="make_system_aaa_default_address")],
+                        ),
+                        DEF(
+                            "default-interface",
+                            [
+                                DEF(
+                                    IF_NAME,
+                                    name="interface",
+                                    gen="make_system_aaa_default_interface",
+                                )
+                            ],
+                        ),
+                    ],
+                    name="type",
+                    required=True,
+                )
+            ],
         ),
     ],
 )
