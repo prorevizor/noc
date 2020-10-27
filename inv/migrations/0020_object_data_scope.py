ac68cfed40a2875399c0fc9fba27be95cebf9484
Andrey Vertiprahov <aversanta@gmail.com>
2020-10-27 19:58:16 +0500

    Fix typo.

--- inv/migrations/0020_object_data_scope.py
+++ inv/migrations/0020_object_data_scope.py
@@ -19,8 +19,9 @@ class Migration(BaseMigration):
         coll = self.mongo_db["noc.objects"]
         bulk = []
         for doc in coll.find({}, no_cursor_timeout=True):
-            data = doc.get("data")
-            if not data or isinstance(data, list):
+            data = doc.get("data") or {}
+            # if mongo data is {} - unsupported operand type(s) for +=: 'BaseDict' and 'list'
+            if isinstance(data, list):
                 continue  # No data or already converted
             new_data = []
             for mi in data:
@@ -30,7 +31,5 @@ class Migration(BaseMigration):
             if len(bulk) >= self.MAX_BULK_SIZE:
                 coll.bulk_write(bulk)
                 bulk = []
-        # Fix unsupported operand type(s) for +=: 'BaseDict' and 'list'
-        bulk += [UpdateMany({"data": {}}, {"$set": {"data": []}})]
         # Write rest of data
         coll.bulk_write(bulk)
