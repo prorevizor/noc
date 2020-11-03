0ebba0a4c245c9af0da16cb32cb24b8d0c0500db
smile13 <sysfar@gmail.com>
2020-11-03 14:50:55 +0300

    fix black/flake8

--- sa/profiles/Eltex/ESR/get_interface_status_ex.py
+++ sa/profiles/Eltex/ESR/get_interface_status_ex.py
@@ -8,6 +8,7 @@
 # NOC modules
 from noc.sa.profiles.Generic.get_interface_status_ex import Script as BaseScript
 
+
 class Script(BaseScript):
     name = "Eltex.ESR.get_interface_status_ex"
     IFNAME_OID = "IF-MIB::ifDescr"
