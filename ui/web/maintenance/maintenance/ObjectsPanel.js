197403acef758d56168aa9ba7c918f01d33078a7
uncol <d.a.zakharov@gmail.com>
2020-11-30 17:31:02 +0300

    refactoring ObjectsPanel

--- ui/web/maintenance/maintenance/ObjectsPanel.js
+++ ui/web/maintenance/maintenance/ObjectsPanel.js
@@ -1,126 +1,142 @@
 //---------------------------------------------------------------------
 // sa.managedobjectselector ObjectsPanel
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2013 The NOC Project
+// Copyright (C) 2007-2020 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
 console.debug("Defining NOC.maintenance.maintenance.ObjectsPanel");
 
 Ext.define("NOC.maintenance.maintenance.ObjectsPanel", {
-    extend: "NOC.core.ApplicationPanel",
+    extend: "Ext.grid.Panel",
     requires: ["NOC.maintenance.maintenance.ObjectsModel"],
     mixins: [
         "NOC.core.Export"
     ],
     app: null,
     autoScroll: true,
+    stateful: true,
+    autoDestroy: true,
+    stateId: "sa.managedobjectselector-objects",
+    loadMask: true,
+    defaultListenerScope: true,
 
-    initComponent: function() {
-        var me = this;
-
-        me.refreshButton = Ext.create("Ext.button.Button", {
-            text: __("Refresh"),
-            glyph: NOC.glyph.refresh,
-            scope: me,
-            handler: me.onRefresh
-        });
-
-        me.exportButton = Ext.create("Ext.button.Button", {
-            tooltip: __("Export"),
-            text: __("Export"),
-            glyph: NOC.glyph.arrow_down,
-            scope: me,
-            handler: me.onExport
-        });
-
-        me.totalField = Ext.create("Ext.form.field.Display");
-
-        me.store = Ext.create("Ext.data.Store", {
-            model: "NOC.maintenance.maintenance.ObjectsModel"
-        });
-
-        me.grid = Ext.create("Ext.grid.Panel", {
-            store: me.store,
-            stateful: true,
-            stateId: "sa.managedobjectselector-objects",
-            columns: [
+    store: {
+        model: "NOC.maintenance.maintenance.ObjectsModel",
+        autoLoad: false,
+        pageSize: 70,
+        leadingBufferZone: 70,
+        numFromEdge: Math.ceil(70 / 2),
+        trailingBufferZone: 70,
+        purgePageCount: 10,
+        remoteSort: true,
+        sorters: [
+            {
+                property: 'address',
+                direction: 'DESC'
+            }
+        ],
+        proxy: {
+            type: "rest",
+            pageParam: "__page",
+            startParam: "__start",
+            limitParam: "__limit",
+            sortParam: "__sort",
+            reader: {
+                type: "json",
+                rootProperty: "data",
+                totalProperty: "total",
+                successProperty: "success"
+            },
+            writer: {
+                type: "json"
+            }
+        }
+    },
+    columns: [
+        {
+            text: __("Name"),
+            dataIndex: "name",
+            width: 200
+        },
+        {
+            text: __("Managed"),
+            dataIndex: "is_managed",
+            renderer: NOC.render.Bool,
+            width: 50
+        },
+        {
+            text: __("Profile"),
+            dataIndex: "profile",
+            width: 100
+        },
+        {
+            text: __("Address"),
+            dataIndex: "address",
+            width: 100
+        },
+        {
+            text: __("Description"),
+            dataIndex: "description",
+            flex: 1
+        },
+        {
+            text: __("Tags"),
+            dataIndex: "tags",
+            renderer: NOC.render.Tags,
+            width: 150
+        }
+    ],
+    dockedItems: [
+        {
+            xtype: "toolbar",
+            dock: "top",
+            items: [
                 {
-                    text: __("Name"),
-                    dataIndex: "name",
-                    width: 200
+                    text: __("Close"),
+                    glyph: NOC.glyph.arrow_left,
+                    handler: "onClose"
                 },
                 {
-                    text: __("Managed"),
-                    dataIndex: "is_managed",
-                    renderer: NOC.render.Bool,
-                    width: 50
+                    tooltip: __("Export"),
+                    text: __("Export"),
+                    glyph: NOC.glyph.arrow_down,
+                    handler: "onExport"
+
                 },
+                "->",
                 {
-                    text: __("Profile"),
-                    dataIndex: "profile",
-                    width: 100
-                },
-                {
-                    text: __("Address"),
-                    dataIndex: "address",
-                    width: 100
-                },
-                {
-                    text: __("Description"),
-                    dataIndex: "description",
-                    flex: 1
-                },
-                {
-                    text: __("Tags"),
-                    dataIndex: "tags",
-                    renderer: NOC.render.Tags,
-                    width: 150
-                }
-            ],
-            dockedItems: [
-                {
-                    xtype: "toolbar",
-                    dock: "top",
-                    items: [
-                        me.getCloseButton(),
-                        me.refreshButton,
-                        me.exportButton,
-                        "->",
-                        me.totalField
-                    ]
+                    xtype: "displayfield",
+                    fieldLabel: __("Total"),
+                    name: "total",
+                    value: __("loading...")
                 }
             ]
-        });
-        Ext.apply(me, {
-            items: [me.grid]
-        });
-        me.callParent();
-    },
+        }
+    ],
 
     preview: function(record, backItem) {
-        var me = this;
-        me.callParent(arguments);
-        me.grid.mask(__('Loading...'));
-        Ext.Ajax.request({
-            url: "/maintenance/maintenance/" + record.get("id") + "/objects/",
-            method: "GET",
+        var me = this,
+            bi = backItem === undefined ? me.backItem : backItem,
+            store = me.getStore();
+
+        store.getProxy().setUrl("/maintenance/maintenance/" + record.get("id") + "/objects/");
+        store.load({
             scope: me,
-            success: function(response) {
-                var data = Ext.decode(response.responseText);
-                me.grid.setTitle(record.get("subject") + " " + __("objects"));
-                me.store.loadData(data);
-                me.totalField.setValue(__("Total: ") + data.length);
-                me.grid.unmask();
-            },
-            failure: function() {
-                me.grid.unmask();
-                NOC.error(__("Failed to get data"));
+            callback: function() {
+                me.down("[name=total]").setValue(store.getTotalCount());
             }
         });
+        me.currentRecord = record;
+        me.backItem = bi;
+    },
+
+    onClose: function() {
+        var me = this;
+        me.app.showItem(me.backItem);
     },
 
     onExport: function() {
         var me = this;
-        me.save(me.grid, 'affected.csv');
+        me.save(me, 'affected.csv');
     }
 });
