8f16f1489f9edf4ba80745a0aab0c9d757fd6e59
Dmitry Volodin <dv@nocproject.org>
2020-12-08 06:52:39 +0300

    Fix

--- core/inv/path.py
+++ core/inv/path.py
@@ -6,13 +6,15 @@
 # ----------------------------------------------------------------------
 
 # Python modules
-from typing import Optional, Iterable, List, Set, Dict
+from typing import Optional, Iterable, List, Set, Dict, DefaultDict
 from dataclasses import dataclass
 from enum import Enum
+from collections import defaultdict
+from itertools import permutations
 
 # NOC modules
 from noc.inv.models.object import Object
-from noc.inv.models.objectconnection import ObjectConnection, ObjectConnectionItem
+from noc.inv.models.objectconnection import ObjectConnection
 
 
 class ConnAction(int, Enum):
@@ -25,6 +27,13 @@ class ConnAction(int, Enum):
 class PathItem(object):
     obj: Object
     connection: str
+
+
+@dataclass(frozen=True)
+class AdjItem(object):
+    local_name: str
+    remote_object: Object
+    remote_name: str
 
 
 def find_path(
@@ -41,13 +50,14 @@ def find_path(
     :return:
     """
 
-    def get_action(item: ObjectConnectionItem) -> ConnAction:
+    def get_action(ro: Object, r_name: str) -> ConnAction:
         """
         Check peer connection and return action
-        :param item: Object Connection Item
+        :param ro: Object reference
+        :param r_name: connection name
         :return:
         """
-        mc = item.object.get_model_connection(item.name)
+        mc = ro.get_model_connection(r_name)
         if not mc:
             return ConnAction.REJECT
         if not mc.protocols:
@@ -56,15 +66,14 @@ def find_path(
             return ConnAction.FOUND
         return ConnAction.REJECT
 
-    def iter_path(final: PathItem, p: PathItem) -> Iterable[PathItem]:
+    def iter_path(final: PathItem) -> Iterable[PathItem]:
         def iter_reconstruct(pp: PathItem) -> Iterable[PathItem]:
             b = prev.get(pp)
             if b:
                 yield from iter_reconstruct(b)
             yield pp
 
-        yield from iter_reconstruct(p)
-        yield final
+        yield from iter_reconstruct(final)
 
     # Check object connection is exists
     if not obj.has_connection(connection):
@@ -83,7 +92,7 @@ def find_path(
     for c in oc.connection:
         if c.object == obj and c.name == connection:
             continue
-        r = get_action(c)
+        r = get_action(c.object, c.name)
         if r == ConnAction.FOUND:
             # Found
             return [
@@ -97,40 +106,50 @@ def find_path(
     if not wave:
         return None  # No suitable completion
     # Process waves
-    seen: Set[Object] = set()
+    seen: Set[Object] = set()  # Seen objects should never be present in wave
     while wave and max_depth > 0:
-        new_wave: Set[Object] = set()
-        new_seen: Set[Object] = set()
+        new_wave: Set[Object] = set()  # Wave for the next step
+        # Collapse the wave and build adjacency matrix
+        neighbors: DefaultDict[Object, List[AdjItem]] = defaultdict(list)
         for oc in ObjectConnection.objects.filter(connection__object__in=list(wave)):
-            # Find incoming connection
-            incoming: Optional[PathItem] = None
-            for c in oc.connection:
-                pi = PathItem(obj=c.object, connection=c.name)
+            for c1, c2 in permutations(oc.connection, 2):
+                neighbors[c1.object] += [
+                    AdjItem(local_name=c1.name, remote_object=c2.object, remote_name=c2.name)
+                ]
+        # Process the wave
+        for co in wave:
+            # Find incoming
+            incoming: Optional[str] = None
+            for adj in neighbors[co]:
+                pi = PathItem(obj=co, connection=adj.local_name)
                 if pi in prev:
-                    incoming = pi
+                    incoming = adj.local_name
                     break
             if not incoming:
-                return None  # Broken path
-            # Find path continuations
-            for c in oc.connection:
-                new_seen.add(c.object)
-                if c.object in seen:
-                    continue  # Already  tried
-                if c.object in wave:
-                    # Shortest path cannot include the edge of the wave
-                    continue
-                pi = PathItem(obj=c.object, connection=c.name)
-                if pi == incoming:
-                    continue  # Do not glance back
-                r = get_action(c)
-                if r == ConnAction.FOUND:
-                    # Reconstruct path
-                    return list(iter_path(pi, incoming))
-                elif r == ConnAction.PASS:
-                    new_wave.add(c.object)
-                    prev[pi] = incoming
+                # Object in wave, but we cannot remember how we'd reached here.
+                raise RuntimeError("Teleportation detected")
+            # Process candidates
+            for adj in neighbors[co]:
+                if adj.remote_object in seen:
+                    continue  # Already been there, nothing interesting
+                if adj.remote_object in wave:
+                    continue  # Shortest path cannot slide across the edge of wave
+                r = get_action(adj.remote_object, adj.remote_name)
+                if r == ConnAction.PASS or r == ConnAction.FOUND:
+                    # Connect internal slots
+                    pi = PathItem(obj=co, connection=adj.local_name)
+                    prev[pi] = PathItem(obj=co, connection=incoming)
+                    # Connect remote candidate
+                    prev[PathItem(obj=adj.remote_object, connection=adj.remote_name)] = pi
+                    if r == ConnAction.FOUND:
+                        # Reconstruct path
+                        return list(
+                            iter_path(PathItem(obj=adj.remote_object, connection=adj.remote_name))
+                        )
+                    else:  # ConnAction.PASS
+                        new_wave.add(adj.remote_object)
+        seen |= wave
         wave = new_wave
-        seen |= new_seen
         max_depth -= 1
     # Wave is stopped, no path
     return None
