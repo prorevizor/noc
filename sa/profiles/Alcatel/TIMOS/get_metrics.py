9ba1909516e36cd0e6118fe41577f8243e1221ef
bee26 <bee26@ya.ru>
2020-12-17 08:38:57 +0000

    Update get_metrics.py

--- sa/profiles/Alcatel/TIMOS/get_metrics.py
+++ sa/profiles/Alcatel/TIMOS/get_metrics.py
@@ -13,7 +13,7 @@ class Script(GetMetricsScript):
     name = "Alcatel.TIMOS.get_metrics"
 
     @metrics(
-        ["Subscribers | Summary | Slot"],
+        ["Subscribers | Summary"],
         has_capability="BRAS | IPoE",
         volatile=False,
         access="S",
@@ -24,40 +24,25 @@ class Script(GetMetricsScript):
             slot = "slot "
             slot += str(oid2[1])
             self.set_metric(
-                id=("Subscribers | Summary | Slot", None),
-                path=("slot", slot, ""),
+                id=("Subscribers | Summary", None),
+                path=("0", slot, "slot"),
                 value=int(v),
                 multi=True,
             )
-        metric = self.snmp.get("1.3.6.1.4.1.6527.3.1.2.33.5.9.1.2.1")
-        self.set_metric(
-            id=("Subscribers | Summary | Slot", None),
-            path=("slot", "Total Subscribers", ""),
-            value=int(metric),
-            multi=True,
-        )
-
-    @metrics(
-        ["Subscribers | Summary | Port"],
-        has_capability="BRAS | IPoE",
-        volatile=False,
-        access="S",
-    )
-    def get_subscribers_metrics_port_snmp(self, metrics):
         names = {x: y for y, x in self.scripts.get_ifindexes().items()}
         for oid, v in self.snmp.getnext("1.3.6.1.4.1.6527.3.1.2.33.1.104.1.60.1", bulk=False):
             oid2 = oid.split("1.3.6.1.4.1.6527.3.1.2.33.1.104.1.60.1.")
             iface_name = names[int(oid2[1])]
             self.set_metric(
-                id=("Subscribers | Summary | Port", None),
-                path=("port", iface_name, ""),
+                id=("Subscribers | Summary", None),
+                path=("0", iface_name, "port"),
                 value=int(v),
                 multi=True,
             )
         metric = self.snmp.get("1.3.6.1.4.1.6527.3.1.2.33.5.9.1.2.1")
         self.set_metric(
-            id=("Subscribers | Summary | Port", None),
-            path=("port", "Total Subscribers", ""),
+            id=("Subscribers | Summary", None),
+            path=("0", "Total Subscribers", ""),
             value=int(metric),
             multi=True,
         )
