b99c9bc845891a09945d34b333718f022128d985
bee26 <bee26@ya.ru>
2020-12-24 13:16:08 +0000

    Update get_metrics.py

--- sa/profiles/Eltex/TAU/get_metrics.py
+++ sa/profiles/Eltex/TAU/get_metrics.py
@@ -12,13 +12,6 @@ from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript, metr
 class Script(GetMetricsScript):
     name = "Eltex.TAU.get_metrics"
 
-    def convert_bytes(self, mem):
-        if mem[-1] == "M":
-            mem = float(mem[:-1]) * 1024 * 1024
-        if mem[-1] == "K":
-            mem = float(mem[:-1]) * 1024
-        return mem
-
     @metrics(
         ["CPU | Usage"],
         has_capability="SNMP | OID | fxsMonitoring",
@@ -35,33 +28,17 @@ class Script(GetMetricsScript):
         )
 
     @metrics(
-        ["Disk | Free"],
-        has_capability="SNMP | OID | fxsMonitoring",
-        volatile=False,
-        access="S",
-    )
-    def get_disc_free(self, metrics):
-        v = self.snmp.get("1.3.6.1.4.1.35265.1.9.4.0", cached=True)
-        disc_free = self.convert_bytes(v)
-        self.set_metric(
-            id=("Disk | Free", None),
-            path=[""],
-            value=int(disc_free),
-            multi=True,
-        )
-
-    @metrics(
-        ["Memory | Free"],
+        ["Memory | Usage"],
         has_capability="SNMP | OID | fxsMonitoring",
         volatile=False,
         access="S",
     )
     def get_memory_free(self, metrics):
         v = self.snmp.get("1.3.6.1.4.1.35265.1.9.4.0", cached=True)
-        mem_free = self.convert_bytes(v)
+        mem_usage = float(mem[:-2])*446.44
         self.set_metric(
-            id=("Memory | Free", None),
+            id=("Memory | Usage", None),
             path=[""],
-            value=int(mem_free),
+            value=int(mem_usage),
             multi=True,
         )
