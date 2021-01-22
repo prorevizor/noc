f026838b7d6db3d7c86a854698ce477f964de702
Mikhnenko Maksim <sysfar@gmail.com>
2021-01-22 17:54:47 +0300

    fix if_speed and load value

--- pm/handlers/value.py
+++ pm/handlers/value.py
@@ -6,6 +6,21 @@
 # ----------------------------------------------------------------------
 
 
+def humanize_load(speed, type_speed):
+
+    d_speed = {
+        "bit/s": 1000000,
+        "kbit/s": 1000,
+    }
+
+    if speed < 1000 and speed > 0:
+        return "%s " % speed
+    res = d_speed.get(type_speed)
+    if speed >= res:
+        print(speed // res)
+        return speed // res
+
+
 def bw_percent(v, in_speed=None, out_speed=None, bandwidth=None, **kwargs):
     """
     Convert speed to speed to bandwidth percent ratio
@@ -17,9 +32,9 @@ def bw_percent(v, in_speed=None, out_speed=None, bandwidth=None, **kwargs):
     :return:
     """
     if bandwidth:
-        return v * 100 / bandwidth
+        return round(humanize_load(int(v), "bit/s") * 100 / humanize_load(bandwidth, "kbit/s"), 0)
     if in_speed:
-        return v * 100 / in_speed
+        return round(humanize_load(int(v), "bit/s") * 100 / humanize_load(in_speed, "kbit/s"), 0)
     if out_speed:
-        return v * 100 / out_speed
+        return round(humanize_load(int(v), "bit/s") * 100 / humanize_load(out_speed, "kbit/s"), 0)
     return v
