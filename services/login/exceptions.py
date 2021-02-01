070490d72553c02a98f1846d7b2ba1e9bc830fdd
Vladimir Komarov <vkomarov@satel.org>
2021-02-01 14:55:05 +0700

    vak-1501 changed messages exceptions of method /api/login/token

--- services/login/exceptions.py
+++ services/login/exceptions.py
@@ -19,7 +19,7 @@ class StarletteHTTPException(Exception):
 
     def __repr__(self) -> str:
         class_name = self.__class__.__name__
-        return f"{class_name}(status_code={self.status_code!r}, detail={self.error!r})"
+        return f"{class_name}(status_code={self.status_code!r}, error={self.error!r})"
 
 
 class HTTPException(StarletteHTTPException):
--- services/login/paths/token.py
+++ services/login/paths/token.py
@@ -11,13 +11,14 @@ from typing import Optional, Dict
 import codecs
 
 # Third-party modules
-from fastapi import APIRouter, Request, HTTPException, Header
+from fastapi import APIRouter, Request, Header
 
 # NOC modules
 from noc.config import config
 from noc.core.comp import smart_text, smart_bytes
 from ..models.token import TokenRequest, TokenResponse
 from ..auth import authenticate, get_jwt_token, get_user_from_jwt, revoke_token, is_revoked
+from ..exceptions import HTTPException
 
 router = APIRouter()
 
@@ -32,11 +33,11 @@ async def token(
     if req.grant_type == "refresh_token":
         # Refresh token
         if is_revoked(req.refresh_token):
-            raise HTTPException(detail="Token is expired", status_code=HTTPStatus.FORBIDDEN)
+            raise HTTPException(error="invalid_grant", status_code=HTTPStatus.FORBIDDEN)
         try:
             user = get_user_from_jwt(req.refresh_token, audience="refresh")
         except ValueError as e:
-            raise HTTPException(detail="Access denied (%s)" % e, status_code=HTTPStatus.FORBIDDEN)
+            raise HTTPException(error="unauthorized_client (%s)" % e, status_code=HTTPStatus.FORBIDDEN)
         revoke_token(req.refresh_token)
         return get_token_response(user)
     elif req.grant_type == "password":
@@ -47,23 +48,23 @@ async def token(
         schema, data = authorization.split(" ", 1)
         if schema != "Basic":
             raise HTTPException(
-                detail="Basic authorization header required", status_code=HTTPStatus.BAD_REQUEST
+                error="unsupported_grant_type", status_code=HTTPStatus.BAD_REQUEST
             )
         auth_data = smart_text(codecs.decode(smart_bytes(data), "base64"))
         if ":" not in auth_data:
             raise HTTPException(
-                detail="Invalid basic auth header", status_code=HTTPStatus.BAD_REQUEST
+                error="invalid_request", status_code=HTTPStatus.BAD_REQUEST
             )
         user, password = auth_data.split(":", 1)
         auth_req = {"user": user, "password": password, "ip": request.client.host}
     else:
-        raise HTTPException(detail="Invalid grant type", status_code=HTTPStatus.BAD_REQUEST)
+        raise HTTPException(error="unsupported_grant_type", status_code=HTTPStatus.BAD_REQUEST)
     # Authenticate
     if auth_req:
         user = authenticate(auth_req)
         if user:
             return get_token_response(user)
-    raise HTTPException(detail="Access denied", status_code=HTTPStatus.BAD_REQUEST)
+    raise HTTPException(error="invalid_scope", status_code=HTTPStatus.BAD_REQUEST)
 
 
 def get_token_response(user: str) -> TokenResponse:
