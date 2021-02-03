aa5e45c047ef1cded9a93b622efa1bbd75d28bf3
Vladimir Komarov <vkomarov@satel.org>
2021-02-03 20:08:46 +0700

    vak-1501 changed raise HTTPException -> return JSONResponse.

--- services/login/paths/token.py
+++ services/login/paths/token.py
@@ -12,13 +12,13 @@ import codecs
 
 # Third-party modules
 from fastapi import APIRouter, Request, Header
+from starlette.responses import JSONResponse
 
 # NOC modules
 from noc.config import config
 from noc.core.comp import smart_text, smart_bytes
 from ..models.token import TokenRequest, TokenResponse
 from ..auth import authenticate, get_jwt_token, get_user_from_jwt, revoke_token, is_revoked
-from ..exceptions import HTTPException
 
 router = APIRouter()
 
@@ -33,12 +33,12 @@ async def token(
     if req.grant_type == "refresh_token":
         # Refresh token
         if is_revoked(req.refresh_token):
-            raise HTTPException(error="invalid_grant", status_code=HTTPStatus.FORBIDDEN)
+            return JSONResponse(content={"error": "invalid_grant"}, status_code=HTTPStatus.FORBIDDEN)
         try:
             user = get_user_from_jwt(req.refresh_token, audience="refresh")
         except ValueError as e:
-            raise HTTPException(
-                error="unauthorized_client (%s)" % e, status_code=HTTPStatus.FORBIDDEN
+            return JSONResponse(
+                content={"error": "unauthorized_client (%s)" % e}, status_code=HTTPStatus.FORBIDDEN
             )
         revoke_token(req.refresh_token)
         return get_token_response(user)
@@ -49,20 +49,20 @@ async def token(
         # CCGrantRequest + Basic auth header
         schema, data = authorization.split(" ", 1)
         if schema != "Basic":
-            raise HTTPException(error="unsupported_grant_type", status_code=HTTPStatus.BAD_REQUEST)
+            return JSONResponse(content={"error": "unsupported_grant_type"}, status_code=HTTPStatus.BAD_REQUEST)
         auth_data = smart_text(codecs.decode(smart_bytes(data), "base64"))
         if ":" not in auth_data:
-            raise HTTPException(error="invalid_request", status_code=HTTPStatus.BAD_REQUEST)
+            return JSONResponse(content={"error": "invalid_request"}, status_code=HTTPStatus.BAD_REQUEST)
         user, password = auth_data.split(":", 1)
         auth_req = {"user": user, "password": password, "ip": request.client.host}
     else:
-        raise HTTPException(error="unsupported_grant_type", status_code=HTTPStatus.BAD_REQUEST)
+        return JSONResponse(content={"error": "unsupported_grant_type"}, status_code=HTTPStatus.BAD_REQUEST)
     # Authenticate
     if auth_req:
         user = authenticate(auth_req)
         if user:
             return get_token_response(user)
-    raise HTTPException(error="invalid_scope", status_code=HTTPStatus.BAD_REQUEST)
+    return JSONResponse(content={"error": "invalid_scope"}, status_code=HTTPStatus.BAD_REQUEST)
 
 
 def get_token_response(user: str) -> TokenResponse:
