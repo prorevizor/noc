2209d50f018c279d2a0745aabd783b6fbfbe9748
Mikhnenko Maksim <sysfar@gmail.com>
2021-03-21 18:02:14 +0300

    fix view

--- services/web/apps/fm/alarm/views.py
+++ services/web/apps/fm/alarm/views.py
@@ -803,15 +803,17 @@ class AlarmApplication(ExtApplication):
             for x in data:
                 x["isInMaintenance"] = x["managed_object"] in mtc
         else:
-            mos = [x["managed_object"] for x in data]
+            mos = set([x["managed_object"] for x in data])
             mtc = {}
-            for mo in mos:
-                mtc[mo] = []
+            for mo in list(mos):
+                interval = []
                 for ao in AffectedObjects._get_collection().find(
                     {"affected_objects.object": {"$eq": mo}}, {"_id": 0, "maintenance": 1}
                 ):
                     m = Maintenance.get_by_id(ao["maintenance"])
-                    mtc[mo] += [(m.start, m.stop)]
+                    interval += [(m.start, m.stop)]
+                if interval:
+                    mtc[mo] = interval
             for x in data:
                 if x["managed_object"] in mtc:
                     left, right = list(zip(*mtc[x["managed_object"]]))
