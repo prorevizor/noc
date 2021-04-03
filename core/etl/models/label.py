edfcb30fa7134a24c53135544d86d403b721fc46
Andrey Vertiprahov <aversanta@gmail.com>
2021-04-03 11:35:36 +0500

    Fix clean_bool on base ETL loader.

--- core/etl/models/label.py
+++ core/etl/models/label.py
@@ -15,6 +15,19 @@ from .base import BaseModel
 class Label(BaseModel):
     id: str
     name: str
+    is_protected: Optional[bool]
     description: Optional[str]
+    enable_division: Optional[bool]
+    enable_managedobject: Optional[bool]
+    enable_managedobjectprofile: Optional[bool]
+    enable_administrativedomain: Optional[bool]
+    enable_authprofile: Optional[bool]
+    enable_networksegment: Optional[bool]
+    enable_resourcegroup: Optional[bool]
+    enable_object: Optional[bool]
+    enable_service: Optional[bool]
+    enable_serviceprofile: Optional[bool]
+    enable_subscriber: Optional[bool]
+    enable_subscriberprofile: Optional[bool]
 
     _csv_fields = ["id", "name", "description"]
