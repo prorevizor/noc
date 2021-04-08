f23d4324fe11c11e280c6f69c49f28b5f21de1ab
Vladimir Komarov <vkomarov@satel.org>
2021-04-08 21:20:15 +0700

    vak-1550 created migration and added ping_time_expr_policy into cfgping datastream.

---
+++ sa/migrations/0215_managedobjectprofile_ping_time_expr_policy.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2021 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.migration.base import BaseMigration
+from django.db import models
+
+
+class Migration(BaseMigration):
+
+    def migrate(self):
+        self.db.add_column(
+            "sa_managedobjectprofile",
+            "ping_time_expr_policy",
+            models.CharField(
+                "Box Running Policy",
+                choices=[("D", "Disable ping"), ("E", "Enable ping but dont follow status")],
+                max_length=1,
+                default="D",
+            ),
+        )
