e319558f0149383d2ccf63a2b11169e802baf8ec
Dmitry Volodin <dv@nocproject.org>
2021-04-29 12:08:04 +0300

    ui: REST API filters

---
+++ services/ui/utils/rest/op.py
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------------
+# Dependency operations
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2021 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+from abc import ABCMeta, abstractmethod
+from typing import Callable
+
+
+class ListOp(metaclass=ABCMeta):
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def get_transform(self, value) -> Callable:
+        def inner(qs):
+            return qs
+
+        return inner
+
+
+class FilterExact(ListOp):
+    def get_transform(self, value):
+        def inner(qs):
+            return qs.filter(**{self.name: value})
+
+        return inner
